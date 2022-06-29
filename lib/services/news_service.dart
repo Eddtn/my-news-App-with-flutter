3acbce17564355d9a323eb63f6388e8fc136f049
eddtn <eddietonsagie@gmail.com>
2022-06-29 16:38:33 +0100

    help in service

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -8,14 +8,18 @@ import 'package:http/http.dart' as http;
 class NewsService {
   Future<List<Article>> getnews() async {
     try {
-      http.Response response;
-      await http.get(
+      http.Response response = await http.get(
         Uri.parse(
           'https://newsapi.org/v2/everything?q=Apple&from=2022-06-28&sortBy=popularity&apiKey=d101e92983a046f0a67d81497a57d84b',
         ),
       );
       print('object people');
-      return getnews();
+      // print(response.body);
+      final data = jsonDecode(response.body);
+      return (data as List)
+          .map<Article>(
+              (element) => Article.fromJson(element as Map<String, dynamic>))
+          .toList();
     } catch (e) {
       print(e);
       rethrow;
--- lib/views/homeview.dart
+++ lib/views/homeview.dart
@@ -14,7 +14,12 @@ class HomeViewPage extends StatefulWidget {
 
 class _HomeViewPageState extends State<HomeViewPage> {
   List<Article> news = [];
-  bool isLoaded = false;
+  bool isLoaded = true;
+
+  // gethere() {
+  //   String data = getnews();
+  //   return data;
+  // }
 
   @override
   void initState() {
@@ -25,7 +30,7 @@ class _HomeViewPageState extends State<HomeViewPage> {
   getnews() async {
     try {
       setState(() {
-        isLoaded = true;
+        isLoaded = false;
       });
       final articles = NewsService();
       news = await articles.getnews();
@@ -39,146 +44,153 @@ class _HomeViewPageState extends State<HomeViewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return isLoaded
+        ? const Center(child: CircularProgressIndicator())
+        : SafeArea(
+            child: Scaffold(
+              body: ListView(
                 children: [
-                  const Text(
-                    'Find Best news \n For Today',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          onSubmitted: (value) {
-                            // value = searchTextController.text;
-                          },
-                          // controller: searchTextController,
-                          decoration: const InputDecoration(
-                            hintText: 'search Destination',
-                            border: InputBorder.none,
-                            prefixIcon: Icon(Icons.search),
-                            // suffixIcon: Icon(Icons.add_a_photo_outlined),
-                          ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Find Best news \n For Today',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      Container(
-                        child: const Icon(
-                          Icons.menu_open_sharp,
-                          color: Color(0xff51C98B),
+                        const SizedBox(
+                          height: 10,
                         ),
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                            color: const Color(0xffE7F4F0),
-                            borderRadius: BorderRadius.circular(5)),
-                      ),
-                    ],
-                  ),
-                  const NewsList(),
-                  const Text(
-                    'Trending News',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ...List.generate(
-                    10,
-                    (index) => InkWell(
-                      onTap: (() => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DatailedPage()))),
-                      child: Stack(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              height: 450,
-                              width: MediaQuery.of(context).size.width,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                  // color: Colors.black.withOpacity(0.9),
-                                  borderRadius: BorderRadius.circular(20)),
-                              child: CachedNetworkImage(
-                                  fit: BoxFit.cover,
-                                  imageUrl:
-                                      'https://media.istockphoto.com/photos/herbs-and-spices-for-cooking-on-dark-background-picture-id941858854?k=20&m=941858854&s=612x612&w=0&h=c6_v6jU2u8hbbxIhldg4TtY5BYzn0i2UovFdAr-xAUU='),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              height: 450,
-                              width: MediaQuery.of(context).size.width,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                  color: Colors.black.withOpacity(0.5),
-                                  borderRadius: BorderRadius.circular(20)),
-                            ),
-                          ),
-                          const Positioned(
-                              // top: 34,
-                              bottom: 120,
-                              left: 30,
-                              child: Text(
-                                'title',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 25),
-                              )),
-                          const Positioned(
-                              bottom: 100,
-                              left: 30,
-                              child: Text(
-                                'author',
-                                style: TextStyle(
-                                  color: Colors.white,
+                        Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                onSubmitted: (value) {
+                                  // value = searchTextController.text;
+                                },
+                                // controller: searchTextController,
+                                decoration: const InputDecoration(
+                                  hintText: 'search Destination',
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(Icons.search),
+                                  // suffixIcon: Icon(Icons.add_a_photo_outlined),
                                 ),
-                              )),
-                          const Positioned(
-                              bottom: 80,
-                              left: 30,
-                              child: Text(
-                                'Discription',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              )),
-                          Positioned(
-                            top: 30,
-                            right: 30,
-                            child: Container(
+                              ),
+                            ),
+                            Container(
+                              child: const Icon(
+                                Icons.menu_open_sharp,
+                                color: Color(0xff51C98B),
+                              ),
                               height: 40,
                               width: 40,
                               decoration: BoxDecoration(
-                                  color: const Color(0xffB48665),
+                                  color: const Color(0xffE7F4F0),
                                   borderRadius: BorderRadius.circular(5)),
-                              child: const Icon(
-                                Icons.bookmark,
-                                color: Color(0xffFEFEFE),
-                              ),
+                            ),
+                          ],
+                        ),
+                        const NewsList(),
+                        const Text(
+                          'Trending News',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ...List.generate(
+                          news.length,
+                          (index) => InkWell(
+                            onTap: (() => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => DatailedPage()))),
+                            child: Stack(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    height: 450,
+                                    width: MediaQuery.of(context).size.width,
+                                    clipBehavior: Clip.antiAlias,
+                                    decoration: BoxDecoration(
+                                        // color: Colors.black.withOpacity(0.9),
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    child: CachedNetworkImage(
+                                      fit: BoxFit.cover,
+                                      imageUrl: "${news[index].urlToImage}",
+                                      // 'https://media.istockphoto.com/photos/herbs-and-spices-for-cooking-on-dark-background-picture-id941858854?k=20&m=941858854&s=612x612&w=0&h=c6_v6jU2u8hbbxIhldg4TtY5BYzn0i2UovFdAr-xAUU='
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    height: 450,
+                                    width: MediaQuery.of(context).size.width,
+                                    clipBehavior: Clip.antiAlias,
+                                    decoration: BoxDecoration(
+                                        color: Colors.black.withOpacity(0.5),
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                  ),
+                                ),
+                                Positioned(
+                                    // top: 34,
+                                    bottom: 120,
+                                    left: 30,
+                                    child: Text(
+                                      "${news[index].title}",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 25),
+                                    )),
+                                Positioned(
+                                    bottom: 100,
+                                    left: 30,
+                                    child: Text(
+                                      "${news[index].author}",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    )),
+                                Positioned(
+                                    bottom: 80,
+                                    left: 30,
+                                    child: Text(
+                                      "${news[index].description}",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    )),
+                                Positioned(
+                                  top: 30,
+                                  right: 30,
+                                  child: Container(
+                                    height: 40,
+                                    width: 40,
+                                    decoration: BoxDecoration(
+                                        color: const Color(0xffB48665),
+                                        borderRadius: BorderRadius.circular(5)),
+                                    child: const Icon(
+                                      Icons.bookmark,
+                                      color: Color(0xffFEFEFE),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
-                      ),
+                        )
+                      ],
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
   }
 }
